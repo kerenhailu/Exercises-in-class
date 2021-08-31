fcd61d983bd5423af06de00ad3b9eb262ab842d8
kerenhailu <keren4509@gmail.com>
2021-08-31 09:35:39 +0300

    Exercises

---
+++ main.js
@@ -0,0 +1,33 @@
+// var foo , bar
+// foo = prompt("number") 
+// if (foo == 5) {
+//     console.log("i am equal to 5");
+// }
+// else {
+//     console.log("i am not equal to 5");
+// }
+// // -------------------------------------------
+// bar = prompt("number")
+// if (foo == bar) {
+//     console.log("bar = foo");
+// }
+// else {
+//     console.log("bar != foo");
+// }
+// // ---------------------------------------------
+//  if (foo<10){
+//     console.log("foo - 10 true");
+// }
+// else {
+//     console.log("false ");
+// }
+// ---------------------------------------------
+var foo;
+foo = Number(prompt("number"))  
+if (foo == foo) {
+    console.log("good");
+}else {
+    console.log("not good");
+}
+// ----------------------------------------------
+
